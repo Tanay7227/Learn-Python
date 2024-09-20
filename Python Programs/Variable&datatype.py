e3e41021a4b8837e0932f4c8f8f293c89b4dfaf7
Tanay Mahale <155952211+Tanay7227@users.noreply.github.com>
2024-09-20 23:47:56 +0530

    Add files via upload

---
+++ Python Programs/Variable&datatype.py
@@ -0,0 +1,23 @@
+# How to Print 
+age = 20
+name = "Tanay Mahale"
+a = 10
+b = 20
+c= 10.5
+d = True
+e = None
+sum = a+b
+
+print(age)
+print(name)
+print(sum)
+
+print("________________________________")
+
+#what Type of datatype
+print(type(age))
+print(type(name))
+print(type(sum))
+print(type(c))
+print(type(d))
+print(type(e))
