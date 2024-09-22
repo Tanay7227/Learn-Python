7f219a1f97affe8b25004a639a0da05f1cf38d45
Tanay Mahale <155952211+Tanay7227@users.noreply.github.com>
2024-09-22 22:33:43 +0530

    Add files via upload

---
+++ Python Programs/input.py
@@ -0,0 +1,23 @@
+# name = input ("Enter Your name:")
+# print ("You Entered This Name:",name)
+
+print("-----------------")
+
+# We convert input String into interger and print at last with its type.
+
+# val =int((input ("Enter Your age:"))) #we can also convert into float str using float(), str().
+# print(type(val))
+# print("You Entered this thing:",val)
+
+name = input("Enter Your Name:")
+age = int(input ("Enter Your age:"))
+Marks = float(input ("Enter Your Marks:"))
+
+print ("Welcome",name)
+print ("Here is Your age:",age)
+print ("Here is Your Marks:",Marks)
+
+'''Note:- when we write any code or program we have 
+          to always takecare about there value types like
+          number is known as int,Name is known as a Str,
+          Decimal Number is recognize as Float structure.'''
