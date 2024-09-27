881ac2d87df43c754fec24a5c047625d6305fef1
Tanay Mahale <155952211+Tanay7227@users.noreply.github.com>
2024-09-27 22:51:32 +0530

    Add files via upload

---
+++ Practice_Questions/Input2num2sum.py
@@ -0,0 +1,39 @@
+# Question:- Write a Program to input 2 Numbers & Print their Sum.
+
+# a = int(input ("Enter Your First Number:"))
+# b = int(input ("Enter Your Second Number:"))
+# print ("Here is Your Result:",a + b)
+
+
+
+# Question:- Write a Program to input side of a square & Print its area.
+
+# a = float(input("Enter Your Sides:- "))
+# print ("Area of Square is:",a*a)
+
+# # -----------or-----------
+# a = float(input("Enter Your Sides:- "))
+# print ("Area of Square is:",a**2)
+
+
+
+#Question:- WAP to input 2 floating point number & print their average.
+
+# a = float(input("Enter Your First Number:"))
+# b = float(input("Enter Your Second Number:"))
+# print("Here is your Result:",(a+b)/2) # Brackets Solve First 
+
+# '''As in given problem i get some knowlegde about brackets 
+# a+b/2 answer is different
+# (a+b)/2 answer is different because of brackets solve first
+# it has more priority. So its Give Correct answer'''
+
+
+
+# WAP to input 2 int numbers,a and b. Print True if a is greater than or equal to b. if not print false.
+
+a = int(input("Enter a:"))
+b = int(input("Enter b:"))
+
+print (a >= b) 
+
