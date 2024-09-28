a09499b9219919451565e76673a8f793ac4ecae3
Tanay Mahale <155952211+Tanay7227@users.noreply.github.com>
2024-09-29 00:35:53 +0530

    Add files via upload

---
+++ Python Programs/Loops.py
@@ -0,0 +1,60 @@
+#Loops 
+'''Loops are used to repeat instructions.
+Type of loops:-
+While loop & For loop
+
+1) While loops:- While condition : 
+-loop runs intile condition is true'''
+
+#Print Hello
+# count = 1 #variable call iterator
+# while count <=5 :
+#     print ("hello") #iteration
+#     count += 1
+
+'''
+2) Break and Continue
+-Break:- Used to terminates the loop when the value become true.
+-Continue:- it skip the current condition value and print other values
+'''
+#Print the number 1 to 10 using break
+# i = 1
+# while i <= 5:
+#     print(i)
+#     if (i == 3):
+#         break
+#     i += 1
+
+#Print the number 1 to 10 using Continue
+# i = 1
+# while i <= 10:
+#     if (i == 3):
+#         i += 1
+#         continue
+#     print (i)
+#     i += 1 
+# print ("End of Loop")
+
+#Print the odd Number using Continue
+# i = 1
+# while i <= 10:
+#     if (i%2 == 0):
+#         i += 1
+#         continue
+#     print (i)
+#     i += 1 
+# print ("End of Loop")
+
+#Print the Even Number using Continue
+# i = 1
+# while i <= 10:
+#     if (i%2 != 0):
+#         i += 1
+#         continue
+#     print (i)
+#     i += 1 
+# print ("End of Loop")
+
+'''
+3) For Loop :- Used for Sequential Traversal.For Traversing list,String,Tuples etc..
+'''
